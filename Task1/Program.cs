c0bcbf59d51080be98f33da657c99dc7af813251
Maryna <maryna.lysyak@gmail.com>
2023-02-10 19:02:24 +0300

    Функция.Возведение числа а в степень в

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,12 +3,29 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-System.Console.WriteLine("Введите число A: ");
-int a = Convert.ToInt32(System.Console.ReadLine());
+// System.Console.WriteLine("Введите число A: ");
+// int a = Convert.ToInt32(System.Console.ReadLine());
 
-System.Console.WriteLine("Введите число B: ");
-int b = Convert.ToInt32(System.Console.ReadLine());
+// System.Console.WriteLine("Введите число B: ");
+// int b = Convert.ToInt32(System.Console.ReadLine());
 
-double c = Math.Pow(a, b);
-System.Console.WriteLine($"{a} в степени {b} = {c}");
+// double c = Math.Pow(a, b), 4;
+// System.Console.WriteLine($"{a} в степени {b} = {c}");
 
+// С функциями:
+
+int GetNum(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+double Result(int a, int b)
+{
+    double res = Math.Pow(a,b);
+    return res;
+}
+
+int numberA = GetNum("Введите число А: ");
+int numberB = GetNum("Введите число В: ");
+System.Console.WriteLine($"Число { numberA} в степени {numberB} = {Result(numberA, numberB)}");
