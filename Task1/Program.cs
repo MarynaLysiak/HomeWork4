ba47215d3e39d67afed36ae9b41c56aa2e2f8d11
Maryna <maryna.lysyak@gmail.com>
2023-02-10 16:54:40 +0300

    Возведение числа а в степень b

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+System.Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(System.Console.ReadLine());
+
+double c = Math.Pow(a, b);
+System.Console.WriteLine($"{a} в степени {b} = {c}");
+
