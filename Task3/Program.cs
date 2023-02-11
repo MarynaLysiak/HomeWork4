120167a87d85a01fbf8720db97617f418818a936
Maryna <maryna.lysyak@gmail.com>
2023-02-12 00:16:55 +0300

    Рандомный массив из 8 элементов с верхней и нижней границами

---
+++ Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void PrintArray(int[] arr)  
+{
+    int count = arr.Length;
+    for (int i = 0; i <= count; i++)
+    {
+       System.Console.Write($"{arr[i]}"+" "); 
+    }
+}
+
+int[] GetArray(int size, int leftRange, int rightRange) 
+{
+    int[] arrN = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arrN[i] = new Random().Next(leftRange, rightRange); 
+    }
+    return arrN;
+}
+int[] myArray = GetArray(8, 3, 100); 
+PrintArray(myArray); 
