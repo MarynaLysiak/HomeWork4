be0bc8bd3230c5e8b8af17da558e44cffe06633d
Maryna <maryna.lysyak@gmail.com>
2023-02-11 23:24:58 +0300

    Программа выдает сумму цифр заданного числа

---
+++ Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Матемитическое решение:
+
+// System.Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(System.Console.ReadLine());
+
+// int sum = 0;
+// int length = Convert.ToString(number);
+// for (int i = 0; i <= length; i++)
+// {
+//     sum = sum + number%10;
+//     number = number/10;
+// }
+
+//  System.Console.WriteLine($"Сумма цифр в числе {number} = {sum}");
+
+// Функция:
+
+int GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+int GetSum(int number)
+{
+    int sum = 0;
+    int length = Convert.ToString(number).Length;
+    for (int i = 0; i <= length; i++)
+    {
+        sum = sum + number%10;
+        number = number/10;
+    }
+    return sum;
+}
+
+int num = GetNumber("Введите число N: ");
+System.Console.WriteLine($"Сумма цифр числа {num} = {GetSum(num)}");
